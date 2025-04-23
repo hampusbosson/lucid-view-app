af8eb80868439da1eefec548315b879eef5ff551
Hampus Bosson <hampus.bosson@outlook.com>
2025-04-23 18:58:08 +0200

    initial commit, setup enviroment

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+const PORT = parseInt(process.env.PORT || '4000', 10);
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+app.use(cookieParser());
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
