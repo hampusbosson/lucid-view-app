2bc97d75f7ec14a053a599bc281ee5f4e20d1828
Hampus Bosson <hampus.bosson@outlook.com>
2025-04-27 19:21:27 +0200

    setup api-client for frontend fetching

---
+++ frontend/src/lib/api-client.ts
@@ -0,0 +1,32 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+
+// Extend InternalAxiosRequestConfig to include requiresAuth
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+    requiresAuth?: boolean;
+}
+    
+export const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+});
+
+const authRequestInterceptor = (config: CustomAxiosRequestConfig) => {
+    if (config.headers) {
+        config.headers.Accept = "application/json";
+    }
+
+    // Conditionally add Authorization header if the request requires authentication
+    if (config.requiresAuth) {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+
+    // Conditionally set withCredentials only for authenticated requests
+    config.withCredentials = !!config.requiresAuth;
+
+    return config;
+
+}
+
+apiClient.interceptors.request.use(authRequestInterceptor);
